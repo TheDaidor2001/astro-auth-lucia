39f822ddbfeed4244760750e44255358921d489a
TheDaidor2001 <danielcastillo1552001@hotmail.com>
2024-04-18 10:57:00 +0100

    first commit

---
+++ db/config.ts
@@ -0,0 +1,27 @@
+import { column, defineDb, defineTable } from 'astro:db';
+
+
+const User = defineTable({
+  columns: {
+    id: column.text({primaryKey: true, optional: false, unique: true}),
+    email: column.text({unique: true, optional: false}),
+    password: column.text({optional: true}),
+  }
+})
+
+const Session = defineTable({
+  columns: {
+    id: column.text({optional: false, unique: true}),
+    userId: column.text({optional: false, references: () => User.columns.id}),
+    expiresAt: column.number({optional: false}),
+  }
+})
+
+
+// https://astro.build/db/config
+export default defineDb({
+  tables: {
+    User,
+    Session
+  }
+});
